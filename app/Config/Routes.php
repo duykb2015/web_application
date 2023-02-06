462f7651c976ca54be5a99edcde3c34a25a031b0
naul99 <luanboss012@gmail.com>
2023-02-06 20:26:45 +0700

    pull

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,8 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+
+
 $routes->get('/example', 'Example::index');
 $routes->get('/v1/example', 'Example::text');
 /*
