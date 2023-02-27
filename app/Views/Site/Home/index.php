3df8f5d38113d12450becb16920c3739691ff4b3
naul99 <luanboss012@gmail.com>
2023-02-27 16:42:50 +0700

    update

--- app/Views/Site/Home/index.php
+++ app/Views/Site/Home/index.php
@@ -150,8 +150,8 @@
                         <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
                             <h6 class="text-truncate mb-3"><?= $row['name'] ?></h6>
                             <div class="d-flex justify-content-center">
-                                <h6><?= $row['price'] ?></h6>
-                                <h6 class="text-muted ml-2"><del><?= $row['discount'] ?></del></h6>
+                                <h6><?=number_format($row['price'], 0, '', ',');?> VNĐ</h6>
+                                <h6 class="text-muted ml-2"><del><?= $row['discount'] ?>%</del></h6>
                             </div>
                         </div>
                         <div class="card-footer d-flex justify-content-between bg-light border">
